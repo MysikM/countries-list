ba669fe6d1a1dce3ccb4a782c7415d332e4232fe
Mysik <Mishamysik@gmail.com>
2022-04-17 18:52:02 +0300

    card create

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import axios from "axios";
+import {useState, useEffect} from "react";
+
 import Header from "./components/Header";
 import Controls from "./components/Controls";
 import Main from "./components/Main";
+import {ALL_COUNTRIES} from "./config";
+import List from "./components/List";
+import Card from "./components/Card";
 
 function App() {
+
+    const [countries, setCountries] = useState([]);
+    console.log(countries);
+    useEffect(()=>{
+        axios.get(ALL_COUNTRIES).then
+            (({data}) => setCountries(data));
+    },[])
   return (
     <>
         <Header />
         <Main>
             <Controls />
+            <List>
+                {countries.map((item) => {
+                    const countryInfo = {
+                        img: item.flags.png,
+                        name: item.name.common,
+                        info: [
+                            {title: 'Population', description: item.population.toLocaleString() },
+                            {title: 'Region', description: item.region },
+                            {title: 'Capital', description: item.capital },
+                        ],
+                    };
+                    return (
+                        <Card key={item.name} {...countryInfo} />
+                    )
+                })}
+            </List>
         </Main>
     </>
   );
